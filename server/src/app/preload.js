a9f251e11237de01effbe25590e95000f49740c4
Marcelo Viana <marcelofv12@hotmail.com>
2024-10-20 17:00:34 -0300

    rm js

--- server/src/app/preload.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log('Preload Loaded');
-const renderer_1 = require("electron/renderer");
-renderer_1.contextBridge.exposeInMainWorld('electronAPI', {
-    onUpdateIP: (callback) => renderer_1.ipcRenderer.on('update-ip', (_event, value) => callback(value)),
-});
-//# sourceMappingURL=preload.js.map
