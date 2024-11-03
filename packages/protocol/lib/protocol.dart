568c317f955710b4df002f5561847d2501a904ea
Marcelo Viana <marcelofv12@hotmail.com>
2024-11-03 07:42:34 -0300

    rm base

--- packages/protocol/lib/protocol.dart
+++ packages/protocol/lib/protocol.dart
@@ -1,9 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
-export 'src/protocol_base.dart';
 export 'src/model.dart';
-
-// TODO: Export any libraries intended for clients of this package.
