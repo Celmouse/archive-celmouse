fa4a9c9369b71a8473cf22d864c4e70ee6237f9c
Marcelo Viana <marcelofv12@hotmail.com>
2024-11-09 15:47:21 -0300

    fix unused

--- website/src/app/downloads/page.tsx
+++ website/src/app/downloads/page.tsx
@@ -4,18 +4,15 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { downloadUrls } from "@/lib/download-urls";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 export default function Page() {
-e
-
     return (
         <main>
             <div>
                 <section id="download" className="w-full py-12 md:py-24 lg:py-32">
                     <div className="px-4 md:px-6">
                         <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-12">Download Celmouse</h2>
-                        <Tabs defaultValue={selectedTab} className="w-full max-w-2xl mx-auto">
+                        <Tabs defaultValue='mobile' className="w-full max-w-2xl mx-auto">
                             <TabsList className="grid w-full grid-cols-4">
                                 <TabsTrigger value="windows">Windows {"(BETA)"}</TabsTrigger>
                                 <TabsTrigger value="mac">MacOSX</TabsTrigger>
