6e4b09997af9bb0b5c29fba65acc50842bc71cfc
Marcelo Viana <marcelofv12@hotmail.com>
2024-11-14 16:43:29 -0300

    add settings persistence

---
+++ mobile/lib/src/services/mouse_settings_persistence.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:controller/src/configs/mouse_settings.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MouseSettingsPersistence {
+  static const String _key = 'current_mouse_settings';
+
+  Future<void> saveSettings(MouseSettings settings) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_key, jsonEncode(settings.toJson()));
+  }
+
+  Future<MouseSettings?> loadSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    final json = prefs.getString(_key);
+    if (json == null) return MouseSettings();
+    return MouseSettings.fromJson(jsonDecode(json));
+  }
+
+}
