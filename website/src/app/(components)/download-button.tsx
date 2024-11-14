7c90576cad551283191d51b453684bbdbc3f49d4
Marcelo Viana <marcelofv12@hotmail.com>
2024-11-14 08:59:47 -0300

    Update download-button.tsx

--- website/src/app/(components)/download-button.tsx
+++ website/src/app/(components)/download-button.tsx
@@ -30,10 +30,11 @@ export function DownloadButton() {
     return (
         <div>
             {os === 'Windows' && <Button asChild><Link href={downloadUrls.win}>Download for {os}</Link></Button>}
-            {os === 'Mac' && <Button asChild><Link href={downloadUrls.macOS}>Download para {os}</Link></Button>}
-            {os === 'Linux' && <Button asChild disabled><Link href={downloadUrls.linux}>Download para {os}</Link></Button>}
-            {os === 'Android' && <Button asChild><Link href={downloadUrls.android}>Download para {os}</Link></Button>}
-            {os === 'iOS' && <Button asChild><Link href={downloadUrls.ios}>Download para {os}</Link></Button>}
+            {os === 'Mac' && <Button asChild><Link href={downloadUrls.macOS}>Download for {os}</Link></Button>}
+            {/* {os === 'Linux' && <Button asChild disabled><Link href={downloadUrls.linux}>Download for {os}</Link></Button>} */}
+            {os === 'Linux' && <Button asChild><Link href="/downloads">Download</Link></Button>}
+            {os === 'Android' && <Button asChild><Link href={downloadUrls.android}>Download for {os}</Link></Button>}
+            {os === 'iOS' && <Button asChild><Link href={downloadUrls.ios}>Download for {os}</Link></Button>}
             {os === 'Outro' && <Button asChild><Link href="/downloads">Download</Link></Button>}
         </div>
     );
