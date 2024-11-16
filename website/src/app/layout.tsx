4a22afaa7c2312382e1c84446ded6e16f875b630
Marcelo Viana <marcelofv12@hotmail.com>
2024-11-16 09:23:38 -0300

    fix

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -17,19 +17,6 @@ const myFont = localFont({
 
 })
 
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
   title: "Celmouse",
   description: "You have all the control in your hands",
