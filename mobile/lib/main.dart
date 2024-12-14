e49668ec27a80d6fc1154f757bd8af6eb6d6b0a3
Marcelo Viana <marcelofv12@hotmail.com>
2024-12-14 09:35:51 -0300

    add device lists

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -11,6 +11,11 @@ import 'src/config/dependencies.dart'; // Import the getit.dart file
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   unawaited(MobileAds.instance.initialize());
+  MobileAds.instance.updateRequestConfiguration(RequestConfiguration(testDeviceIds: [
+    '36e2a439-001d-4afb-966d-f7221a95cdaa',
+    '9785EF7F-73AB-4205-9F95-69C9F7423D94',
+    //TODO: [Mathis] Add your test device ID here or send me the IDFA (https://support.google.com/admob/answer/9691433?hl=en#ID)
+  ]));
   await dotenv.load(fileName: ".env");
 
   runApp(
