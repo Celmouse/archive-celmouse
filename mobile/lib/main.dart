e110763804af9f08c346d454ccbdd3497f6e156d
Marcelo Viana <marcelofv12@hotmail.com>
2024-12-14 09:32:58 -0300

    add debug checker

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 
 import 'app.dart';
@@ -7,7 +10,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  unawaited(MobileAds.instance.initialize());
   await dotenv.load(fileName: ".env");
+
+
 
   runApp(
     MultiProvider(
