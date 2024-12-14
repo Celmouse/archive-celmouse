58d57da45c37322c7233e3f257bb13760881d04e
Marcelo Viana <marcelofv12@hotmail.com>
2024-12-14 14:54:54 -0300

    Update ad_helper.dart

--- mobile/lib/src/helpers/ad_helper.dart
+++ mobile/lib/src/helpers/ad_helper.dart
@@ -5,12 +5,10 @@ import 'package:controller/src/config/enviroment.dart';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      print("KEY ==== ${EnviromentVariables.admobBannerIdAndroid}");
       return EnviromentVariables.admobAppIdAndroid;
     } else if (Platform.isIOS) {
       return EnviromentVariables.admobBannerId;
     } else {
-      print("UNSUPPORTED PLATFORM");
       throw UnsupportedError("Unsupported platform");
     }
   }
