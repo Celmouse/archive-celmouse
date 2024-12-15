17ebe5ff091c0d1308a6069a7c239b1f53ef4310
Marcelo Viana <marcelofv12@hotmail.com>
2024-12-15 17:06:07 -0300

    fixed ads

--- mobile/lib/src/ui/mouse/view/mouse_page.dart
+++ mobile/lib/src/ui/mouse/view/mouse_page.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:controller/getit.dart';
 import 'package:controller/src/UI/trackpad/trackpad_page.dart';
-import 'package:controller/src/config/consent_manager.dart';
-import 'package:controller/src/config/enviroment.dart';
+import 'package:controller/src/ui/ads/ui/banner.dart';
 import 'package:controller/src/ui/keyboard/view/keyboard.dart';
 import 'package:controller/src/ui/keyboard/viewmodel/keyboard_view_model.dart';
 import 'package:controller/src/ui/mouse_move/view/mouse_move_body.dart';
@@ -11,7 +8,6 @@ import 'package:controller/src/ui/mouse_move/view/mouse_move_settings_page.dart'
 import 'package:controller/src/ui/mouse/viewmodel/mouse_viewmodel.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 
 import '../../../domain/models/mouse_settings_model.dart';
@@ -40,11 +36,6 @@ class _MousePageState extends State<MousePage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   int _currentPageIndex = 0;
   final PageController _pageController = PageController();
-  final _consentManager = ConsentManager();
-  var _isMobileAdsInitializeCalled = false;
-  var _isPrivacyOptionsRequired = false;
-  BannerAd? _bannerAd;
-  bool _isLoaded = false;
 
   @override
   void initState() {
@@ -54,21 +45,10 @@ class _MousePageState extends State<MousePage> {
       getIt.registerSingleton<MouseSettings>(settings);
     });
 
-    _consentManager.gatherConsent((consentGatheringError) {
-      // Check if a privacy options entry point is required.
-      _getIsPrivacyOptionsRequired();
-
-      // Attempt to initialize the Mobile Ads SDK.
-      _initializeMobileAdsSDK();
-    });
-
-    // This sample attempts to load ads using consent obtained in the previous session.
-    _initializeMobileAdsSDK();
   }
 
   @override
   void dispose() {
-    _bannerAd?.dispose();
     widget.viewmodel.dispose();
     _pageController.dispose();
     super.dispose();
@@ -194,12 +174,7 @@ class _MousePageState extends State<MousePage> {
             const SizedBox(
               height: 12,
             ),
-            if (_bannerAd != null && _isLoaded)
-              SizedBox(
-                width: _bannerAd!.size.width.toDouble(),
-                height: _bannerAd!.size.height.toDouble(),
-                child: AdWidget(ad: _bannerAd!),
-              ),
+            const BannerAdWidget(),
           ],
         ),
       ),
@@ -215,84 +190,5 @@ class _MousePageState extends State<MousePage> {
     );
   }
 
-  /// Loads and shows a banner ad.
-  ///
-  /// Dimensions of the ad are determined by the width of the screen.
-  void _loadAd() async {
-    // Only load an ad if the Mobile Ads SDK has gathered consent aligned with
-    // the app's configured messages.
-    var canRequestAds = await _consentManager.canRequestAds();
-    if (!canRequestAds) {
-      return;
-    }
-
-    if (!mounted) {
-      return;
-    }
-
-    final String _adUnitId = Platform.isAndroid
-        ? EnviromentVariables.admobBannerIdAndroid
-        : EnviromentVariables.admobBannerId;
-
-    // Get an AnchoredAdaptiveBannerAdSize before loading the ad.
-    final size = await AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
-        MediaQuery.sizeOf(context).width.truncate());
-
-    if (size == null) {
-      // Unable to get width of anchored banner.
-      return;
-    }
-
-    BannerAd(
-      adUnitId: _adUnitId,
-      request: const AdRequest(),
-      size: size,
-      listener: BannerAdListener(
-        // Called when an ad is successfully received.
-        onAdLoaded: (ad) {
-          setState(() {
-            _bannerAd = ad as BannerAd;
-            _isLoaded = true;
-          });
-        },
-        // Called when an ad request failed.
-        onAdFailedToLoad: (ad, err) {
-          ad.dispose();
-        },
-        // Called when an ad opens an overlay that covers the screen.
-        onAdOpened: (Ad ad) {},
-        // Called when an ad removes an overlay that covers the screen.
-        onAdClosed: (Ad ad) {},
-        // Called when an impression occurs on the ad.
-        onAdImpression: (Ad ad) {},
-      ),
-    ).load();
-  }
-
-  /// Redraw the app bar actions if a privacy options entry point is required.
-  void _getIsPrivacyOptionsRequired() async {
-    if (await _consentManager.isPrivacyOptionsRequired()) {
-      setState(() {
-        _isPrivacyOptionsRequired = true;
-      });
-    }
-  }
-
-  /// Initialize the Mobile Ads SDK if the SDK has gathered consent aligned with
-  /// the app's configured messages.
-  void _initializeMobileAdsSDK() async {
-    if (_isMobileAdsInitializeCalled) {
-      return;
-    }
-
-    if (await _consentManager.canRequestAds()) {
-      _isMobileAdsInitializeCalled = true;
-
-      // Initialize the Mobile Ads SDK.
-      MobileAds.instance.initialize();
-
-      // Load an ad.
-      _loadAd();
-    }
-  }
+ 
 }
