2e954a5ad3458f68bbed0373891798160af6ad5a
Marcelo Viana <marcelofv12@hotmail.com>
2024-12-22 19:55:18 -0300

    add Draggable component

--- mobile/lib/src/ui/layout_builder/view/layout_builder_page.dart
+++ mobile/lib/src/ui/layout_builder/view/layout_builder_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const bool extendsBodyBehindAppBar = false;
+
 // TODO: Get the position of the tap and place a container there
 // When I do select the container, I should be able to drag it around
 // When I do select the container, a drawer will be opened on the other half of the screen with the
@@ -22,19 +24,11 @@ class _LayoutBuilderPageState extends State<LayoutBuilderPage> {
         x: offset.dx,
         y: offset.dy,
         size: 50,
-        onPositionChanged: (double x, double y) {
-          setState(() {
-            // final element = items.firstWhere((element) => element.id == id);
-            // element.x = x;
-          });
-        },
       ));
     });
   }
 
-  changeItemPosition(String id, double x, double y){
-
-  }
+  changeItemPosition(String id, double x, double y) {}
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +36,7 @@ class _LayoutBuilderPageState extends State<LayoutBuilderPage> {
       appBar: AppBar(
         title: const Text('Tap to Place Container'),
       ),
+      extendBodyBehindAppBar: extendsBodyBehindAppBar,
       body: Stack(
         children: [
           GestureDetector(
@@ -63,31 +58,45 @@ class LayoutBuilderItem extends StatefulWidget {
     required this.x,
     required this.y,
     required this.size,
-    required this.onPositionChanged,
   });
 
   final String id;
   final double x;
   final double y;
   final double size;
-  final Function(double x, double y) onPositionChanged;
 
   @override
   State<LayoutBuilderItem> createState() => _LayoutBuilderItemState();
 }
 
 class _LayoutBuilderItemState extends State<LayoutBuilderItem> {
+  late double x;
+  late double y;
+
+  @override
+  void initState() {
+    x = widget.x - widget.size / 2;
+    y = widget.y - widget.size / 2;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    double x = widget.x - widget.size / 2;
-    double y = widget.y - widget.size / 2;
-    
     return Positioned(
       top: y,
       left: x,
       child: Draggable(
+        rootOverlay: true,
         onDragEnd: (details) {
-          widget.onPositionChanged(details.offset.dx, details.offset.dy);
+          double appBarHeight = 0;
+          if (Scaffold.of(context).hasAppBar && !extendsBodyBehindAppBar) {
+            appBarHeight = Scaffold.of(context).appBarMaxHeight ?? 0;
+          }
+
+          setState(() {
+            x = details.offset.dx;
+            y = details.offset.dy - appBarHeight.toDouble();
+          });
         },
         feedback: Container(
           width: widget.size,
