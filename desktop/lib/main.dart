5f0fcc6d9b2475f28d1ce19a277361ad54a8d475
MathisZerbib <mathis.zerbib@gmail.com>
2025-01-10 01:24:53 +0100

    removed banner debug

--- desktop/lib/main.dart
+++ desktop/lib/main.dart
@@ -1,42 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:server/src/ui/home/view/home_page.dart';
+import 'package:server/src/ui/home/viewmodel/home_viewmodel.dart';
 import 'package:server/src/data/services/connection_service.dart';
 import 'package:server/src/data/services/device_info_service.dart';
 import 'package:server/src/data/services/keyboard_service.dart';
 import 'package:server/src/data/services/mouse_service.dart';
 import 'package:server/src/data/socket_repository.dart';
-import 'package:server/src/ui/home/viewmodel/home_viewmodel.dart';
 
-import 'src/ui/home/view/home_page.dart';
-
-Future<void> main() async {
-  runApp(
-    const MyApp(),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Celmouse HUB',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-        ),
-        useMaterial3: true,
-      ),
-      home: Home(
-        viewmodel: HomeViewmodel(
-          socketRepository: SocketRepository(
-            mouseService: MouseService(),
-            keyboardService: KeyboardService(),
-            deviceInfoService: DeviceInfoService(),
-            connectionService: ConnectionService(),
+    return ScreenUtilInit(
+      designSize: const Size(800, 600),
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Celmouse',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
           ),
-        ),
-      ),
+          home: Home(
+            viewmodel: HomeViewmodel(
+              socketRepository: SocketRepository(
+                mouseService: MouseService(),
+                keyboardService: KeyboardService(),
+                deviceInfoService: DeviceInfoService(),
+                connectionService: ConnectionService(),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
