34d841a4510d6223be88a407dc528ab359b7de7d
Marcelo Viana <marcelofv12@hotmail.com>
2025-01-22 14:38:13 -0300

    keyboard is now working a-z 0-9

--- desktop/lib/src/data/services/keyboard_service.dart
+++ desktop/lib/src/data/services/keyboard_service.dart
@@ -1,23 +1,39 @@
 import 'dart:async';
 
+import 'package:flutter/services.dart';
 import 'package:keyboard/keyboard.dart';
 import 'package:protocol/protocol.dart';
 
 class KeyboardService {
   final keyboard = Keyboard();
+  final _channel = KeyboardPlatformChannel();
 
   void type(String key) {
-    keyboard.pressKey(key);
-    Timer(const Duration(milliseconds: 200), () {
-      keyboard.releaseKey(key);
-    });
+    
+    _channel.pressKey(key);
+
+    // Timer(const Duration(milliseconds: 200), () {
+    //   // keyboard.releaseKey(key);
+    // });
   }
 
   void pressSpecial(SpecialKeyType key) {
-    keyboard.pressSpecialKey(key);
+    // _channel.pressSpecialKey(key);
   }
 
   void releaseSpecial(SpecialKeyType key) {
-    keyboard.releaseSpecialKey(key);
+    // _channel.releaseSpecialKey(key);
+  }
+}
+
+class KeyboardPlatformChannel {
+  static const channel = MethodChannel('com.celmouse.plugins/keyboard');
+
+  Future<void> pressKey(String key) async {
+    await channel.invokeMethod('pressKey', key);
+  }
+
+  Future<void> pressSpecialKey(String key) async {
+    await channel.invokeMethod('pressSpecialKey', key);
   }
 }
--- desktop/macos/Runner/MainFlutterWindow.swift
+++ desktop/macos/Runner/MainFlutterWindow.swift
@@ -1,5 +1,3 @@
-import Cocoa
-import CoreGraphics
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
@@ -8,128 +6,23 @@ class MainFlutterWindow: NSWindow {
         let windowFrame = self.frame
         self.contentViewController = flutterViewController
         self.setFrame(windowFrame, display: true)
-
+        
         let mouseChannel = FlutterMethodChannel(
             name: "com.celmouse.plugins/mouse",
             binaryMessenger: flutterViewController.engine.binaryMessenger
         )
-
-        let mouseController = MouseController()
-
-        mouseChannel.setMethodCallHandler { (call, result) in
-            switch call.method {
-            case "moveTo":
-                if let arguments = call.arguments as? [String: Double],
-                    let x = arguments["x"],
-                    let y = arguments["y"]
-                {
-                    mouseController.moveTo(to: CGPoint(x: x, y: y))
-
-                    result(nil)
-                } else {
-                    result(
-                        FlutterError(
-                            code: "INVALID_ARGUMENTS",
-                            message: "Invalid arguments",
-                            details: nil
-                        )
-                    )
-                }
-            case "move":
-                if let arguments = call.arguments as? [String: Double],
-                    let x = arguments["x"],
-                    let y = arguments["y"]
-                {
-
-                    mouseController.move(x: x, y: y)
-
-                    result(nil)
-                } else {
-                    result(
-                        FlutterError(
-                            code: "INVALID_ARGUMENTS",
-                            message: "Invalid arguments",
-                            details: nil
-                        )
-                    )
-                }
-            case "scroll":
-                if let arguments = call.arguments as? [String: Double],
-                    let x = arguments["x"],
-                    let y = arguments["y"]
-                {
-
-                    mouseController.scroll(x: x, y: y)
-
-                    result(nil)
-                } else {
-                    result(
-                        FlutterError(
-                            code: "INVALID_ARGUMENTS",
-                            message: "Invalid arguments",
-                            details: nil
-                        )
-                    )
-                }
-            case "click":
-                if let arguments = call.arguments as? Int {
-                    mouseController.click(button: arguments)
-                    result(nil)
-                } else {
-                    result(
-                        FlutterError(
-                            code: "INVALID_ARGUMENTS",
-                            message: "Invalid arguments",
-                            details: nil
-                        )
-                    )
-                }
-            case "doubleClick":
-                mouseController.doubleClick()
-                result(nil)
-            case "holdButton":
-                if let arguments = call.arguments as? Int {
-                    mouseController.holdButton(button: arguments)
-                    result(nil)
-                } else {
-                    result(
-                        FlutterError(
-                            code: "INVALID_ARGUMENTS",
-                            message: "Invalid arguments",
-                            details: nil
-                        )
-                    )
-                }
-            case "releaseButton":
-                if let arguments = call.arguments as? Int {
-                    mouseController.releaseButton(button: arguments)
-                    result(nil)
-                } else {
-                    result(
-                        FlutterError(
-                            code: "INVALID_ARGUMENTS",
-                            message: "Invalid arguments",
-                            details: nil
-                        )
-                    )
-                }
-            //            case "screenSize":
-            //                result(self.screenSize())
-            default:
-                result(FlutterMethodNotImplemented)
-            }
-        }
-
+        
+        let _ = MouseController(channel: mouseChannel)
+        
+        let keyboardChannel = FlutterMethodChannel(
+            name: "com.celmouse.plugins/keyboard",
+            binaryMessenger: flutterViewController.engine.binaryMessenger
+        )
+        
+        let _ = KeyboardController(channel: keyboardChannel)
+        
         RegisterGeneratedPlugins(registry: flutterViewController)
-
+        
         super.awakeFromNib()
     }
-
-    //    func screenSize() -> [String : Double] {
-    //        let screenSize: CGRect = UIScreen.main.bounds
-    //        let coordinates = [:]
-    //        coordinates["x"] = screenSize.size.width
-    //        coordinates["y"] = screenSize.size.height
-    //        return coordinates
-    //    }
 }
