2148a34d26d144c553813a9860a7d40fc07eae5d
Marcelo Viana <marcelofv12@hotmail.com>
2025-02-16 19:08:22 -0300

    movement and clicking is working

--- desktop/windows/runner/flutter_window.cpp
+++ desktop/windows/runner/flutter_window.cpp
@@ -1,11 +1,13 @@
+#include <iostream>
 #include "flutter_window.h"
 #include <flutter/event_channel.h>
 #include <flutter/event_sink.h>
 #include <flutter/event_stream_handler_functions.h>
 #include <flutter/method_channel.h>
 #include <flutter/standard_method_codec.h>
-#include <windows.h>
-
+#include <flutter/plugin_registrar_windows.h>
+#include <Windows.h>
+#include <map>
 #include <memory>
 #include <optional>
 
@@ -16,8 +18,10 @@ FlutterWindow::FlutterWindow(const flutter::DartProject &project)
 
 FlutterWindow::~FlutterWindow() {}
 
-bool FlutterWindow::OnCreate() {
-  if (!Win32Window::OnCreate()) {
+bool FlutterWindow::OnCreate()
+{
+  if (!Win32Window::OnCreate())
+  {
     return false;
   }
 
@@ -28,32 +32,70 @@ bool FlutterWindow::OnCreate() {
   flutter_controller_ = std::make_unique<flutter::FlutterViewController>(
       frame.right - frame.left, frame.bottom - frame.top, project_);
   // Ensure that basic setup of the controller was successful.
-  if (!flutter_controller_->engine() || !flutter_controller_->view()) {
+  if (!flutter_controller_->engine() || !flutter_controller_->view())
+  {
     return false;
   }
   RegisterPlugins(flutter_controller_->engine());
   flutter::MethodChannel<> channel(
       flutter_controller_->engine()->messenger(),
-      "com.celmouse.plugins/keyboard",
+      "com.celmouse.plugins/mouse",
       &flutter::StandardMethodCodec::GetInstance());
+
   channel.SetMethodCallHandler(
       [](const flutter::MethodCall<> &call,
-         std::unique_ptr<flutter::MethodResult<>> result) {
-        if (call.method_name() == "getBatteryLevel") {
-          int battery_level = GetBatteryLevel();
-          if (battery_level != -1) {
-            result->Success(battery_level);
-          } else {
-            result->Error("UNAVAILABLE", "Battery level not available.");
+         std::unique_ptr<flutter::MethodResult<>> result)
+      {
+        // call.arguments()
+
+        if (call.method_name().compare("move") == 0)
+        {
+          std::cout << "Ta aqui" << std::endl;
+
+          const auto *arguments = std::get_if<flutter::EncodableMap>(call.arguments());
+          if (arguments)
+          {
+            auto dx = std::get<double>(arguments->at(flutter::EncodableValue("x")));
+            auto dy = std::get<double>(arguments->at(flutter::EncodableValue("y")));
+
+            // POINT cursorPos;
+            // GetCursorPos(&cursorPos);
+            // std::cout << "Cursor Position (" << cursorPos.x << ", " << cursorPos.y << ")" << std::endl;
+            // SetCursorPos(cursorPos.x + static_cast<int>(dx), cursorPos.y + static_cast<int>(dy));
+            ShowCursor(TRUE);
+            // mouse_event(MOUSEEVENTF_MOVE, dx, dy, 0, 0);
+            INPUT input = {0};
+            input.type = INPUT_MOUSE;
+            input.mi.dx = static_cast<LONG>(dx);
+            input.mi.dy = static_cast<LONG>(dy);
+            input.mi.dwFlags = MOUSEEVENTF_MOVE;
+            SendInput(1, &input, sizeof(INPUT));
           }
-        } else {
-          result->NotImplemented();
+          result->Success();
+        }
+        if (call.method_name() == "click")
+        {
+          int button = 0;
+          INPUT input[2] = {};
+          input[0].type = input[1].type = INPUT_MOUSE;
+          if (button == 0)
+          {
+            input[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+            input[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+          }
+          else if (button == 1)
+          {
+            input[0].mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+            input[1].mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+          }
+          SendInput(2, input, sizeof(INPUT));
         }
       });
 
   SetChildContent(flutter_controller_->view()->GetNativeWindow());
 
-  flutter_controller_->engine()->SetNextFrameCallback([&]() { this->Show(); });
+  flutter_controller_->engine()->SetNextFrameCallback([&]()
+                                                      { this->Show(); });
 
   // Flutter can complete the first frame before the "show window" callback is
   // registered. The following call ensures a frame is pending to ensure the
@@ -63,8 +105,10 @@ bool FlutterWindow::OnCreate() {
   return true;
 }
 
-void FlutterWindow::OnDestroy() {
-  if (flutter_controller_) {
+void FlutterWindow::OnDestroy()
+{
+  if (flutter_controller_)
+  {
     flutter_controller_ = nullptr;
   }
 
@@ -74,18 +118,22 @@ void FlutterWindow::OnDestroy() {
 LRESULT
 FlutterWindow::MessageHandler(HWND hwnd, UINT const message,
                               WPARAM const wparam,
-                              LPARAM const lparam) noexcept {
+                              LPARAM const lparam) noexcept
+{
   // Give Flutter, including plugins, an opportunity to handle window messages.
-  if (flutter_controller_) {
+  if (flutter_controller_)
+  {
     std::optional<LRESULT> result =
         flutter_controller_->HandleTopLevelWindowProc(hwnd, message, wparam,
                                                       lparam);
-    if (result) {
+    if (result)
+    {
       return *result;
     }
   }
 
-  switch (message) {
+  switch (message)
+  {
   case WM_FONTCHANGE:
     flutter_controller_->engine()->ReloadSystemFonts();
     break;
